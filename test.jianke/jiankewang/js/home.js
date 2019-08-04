3b1cc699d0f8a836126f9f0b533d9e246783b51f
baishu <wendingding@126.com>
2019-08-04 21:31:26 +0800

    实现了首页商品部分

--- test.jianke/jiankewang/js/home.js
+++ test.jianke/jiankewang/js/home.js
@@ -104,13 +104,13 @@ $(function () {
         }
     })
 
-        //轮播图
+    //轮播图
     $.ajax({
         type: "get",
         url: "banner.json",
         dataType: "json",
-        success: function(data){
-            
+        success: function (data) {
+
             (new BannerManager(data, 'true', 'true', 'true', 1, 1500, 0, $(".box2"))).init();
         }
     })
@@ -119,8 +119,96 @@ $(function () {
         type: "get",
         url: "./sever/main.json",
         dataType: "json",
-        success: function(data){
-          
+        success: function (data) {
+            let html = data.map(function (element) {
+
+                return `<li>
+              <img class="bigimg" src="${element.imgsrc}" alt="">
+              <div class="box3">
+                   <div class="box3-1">
+                        <h3>${element.title}</h3> 
+                        <p>${element.model}</p>  
+                   </div>
+              </div>
+              <ul>
+                                <li>
+                                    <a href="#"><img src="${element.src1}" alt=""></a>
+                                    <h4>${element.name1}</h4>
+                                    <p>${element.des1}</p>
+                                </li>
+                                <li>
+                                <a href="#"><img src="${element.src2}" alt=""></a>
+                                <h4>${element.name2}</h4>
+                                <p>${element.des3}</p>
+                                </li>
+                                <li>
+                                <a href="#"><img src="${element.src3}" alt=""></a>
+                                <h4>${element.name3}</h4>
+                                <p>${element.des3}</p>
+                                </li>
+                            </ul>
+           </li>`
+            }).join('');
+            $('.nav5').html(html);
+        }
+    })
+
+    $.ajax({
+        type: "get",
+        url: "./sever/main1.json",
+        dataType: "json",
+        success: function (data) {
+            let html = data.map(value => {
+                let str = value.models.map(ele => {
+                    return `<li><em></em><a href="#">${ele}</a></li>`
+                }).join('');
+                let str1 = value.urlimg.map(item => {
+                    return `<li><img src="${item}" alt=""></li>`
+                }).join('');
+                return `<div class="floorDiv">
+                <h2>${value.title}</h2>
+                <div class="cigimgs">
+                     <img class="cigimg" src="${value.bigimg}" alt="">
+                    <ul class="nav6-1">
+                        ${str}
+                    </ul>
+                    <ul class="nav6-2">
+                        ${str1}
+                    </ul>
+                </div>
+                <div class="dgimgs">
+                    <img class="dgimg" src="${value.imgsrc}" alt="">
+                    <div class="dgimgs-1">
+                        <h3>${value.name}</h3>
+                        <p>${value.piec}</p>
+                    </div>
+                </div>
+                <ul class="egimgs">
+                    <li><p>${value.des1}</p><img src="${value.src1}" alt=""></li>
+                    <li><p>${value.des2}</p><img src="${value.src2}" alt=""></li>
+                    <li><p>${value.des3}</p><img src="${value.src3}" alt=""></li>
+                    <li><p>${value.des4}</p><img src="${value.src4}" alt=""></li>
+                </ul>
+                <ul class="fgimgs">
+                    <li>
+                        <p>${value.des5}</p>
+                        <img src="${value.src5}" alt="">
+                    </li>
+                    <li>
+                        <p>${value.des6}</p>
+                        <img src="${value.src6}" alt="">
+                    </li>
+                    <li>
+                        <p>${value.des7}</p>
+                        <img src="${value.src7}" alt="">
+                    </li>
+                </ul>
+                <div class="advertisi">
+                    <img src="${value.src8}" alt="">
+                </div>
+            </div>`
+            }).join('');
+            $('.floor').children().html(html);
         }
     })
 })
